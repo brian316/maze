506614a5eda8011eb3f5aa9499fab6d7eacfdd78
Brian Duenas <duenas_brian@yahoo.com>
2017-05-11 14:15:33 -0700

    Update MazeMain.cpp

--- MazeMain.cpp
+++ MazeMain.cpp
@@ -2,43 +2,83 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+map<int,vector<int> > read_maze(string, int&, int&);
+bool traverse_maze(map<int,vector<int> >, int, int);
+
 int main()
 {
-	map<int,vector<int> > maze;
+  map<int,vector<int> > maze;
 
-	int start, goal;
+  int start,goal;
 
-	ifstream inp;
-	inp.open("lab5_maze.txt");
+  maze = read_maze("lab5_maze.txt",start,goal);
 
-	inp >> start >> goal;
 
-	int n;
-	int next;
-	for (int i = start; i <= goal; i++)
-	{
-		inp >> n;
-		vector<int> neighs;
-		for (int j = 1; j <= n; j++)
-		{
-			inp >> next;
-			neighs.push_back(next);
-		}
-		maze[i] = neighs;
-	}
-	inp.close();
+  for (int i = start; i <= goal; i++)
+    {
+      cout << "from " << i << " to [ ";
+      for (int j = 0; j < maze[i].size(); j++)
+	cout << maze[i][j] << " ";
+      cout << " ]" <<  endl;
+    }
 
-	for (int i = start; i <= goal; i++)
-	{
-		cout << "from " << i << " to [ ";
-		for (int j = 0; j < maze[i].size(); j++)
-			cout << maze[i][j] << " ";
-		cout << " ]" <<  endl;
-	}
-	system("Pause");
-	return 0;
+  while (true)
+    {
+      cout << "Immediate neighbors of which node? ";
+      int n;
+      cin >> n;
+      cout << endl;
+
+      vector<int> neighs = maze[n];
+
+      for (int i = 0; i < neighs.size(); i++)
+	cout << neighs[i] << " ";
+      cout << endl << endl;
+    }
+  return 0;
 }
 
+map<int,vector<int> > read_maze(string mfile, int& from, int& to)
+{
+  map<int,vector<int> > maze;
+
+  int start = 0;
+  int goal =0;
+
+  ifstream inp;
+  inp.open(mfile);
+
+  inp >> start >> goal;
+
+  from = start;
+  to = goal;
+
+  int n;
+  int next;
+  for (int i = start; i <= goal; i++)
+    {
+      inp >> n;
+      vector<int> neighs;
+      for (int j = 1; j <= n; j++)
+	{
+	  inp >> next;
+	  neighs.push_back(next);
+	}
+      maze[i] = neighs;
+    }
+  inp.close();
+  return maze;
+}
+
+
+// traverse from start to goal according to algorithm;
+// print out nodes as they are being explored (
+
+bool  traverse_maze(map<int,vector<int> > maze, int start, int goal)
+{
+	
+}
